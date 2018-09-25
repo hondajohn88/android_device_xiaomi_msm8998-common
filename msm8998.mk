e29b11f95d6a14651d399627ee76df119bfb9e36
hondajohn88 <hondajohn88@gmail.com>
2018-09-25 15:40:45 -0700

    Attempt to get proximity sensor working in selinux enforced mode.

--- msm8998.mk
+++ msm8998.mk
@@ -321,7 +321,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
 # USB
---
+++ vndk/vndk-sp-libs.mk
@@ -0,0 +1,4 @@
+VNDK_SP_LIBRARIES := \
+    android.hardware.sensors@1.0 \
+    android.hidl.base@1.0 \
+    android.hidl.memory@1.0
